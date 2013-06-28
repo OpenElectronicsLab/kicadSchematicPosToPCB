3d681638870bf99e20ca5a80a0e901934920a17e
Kendrick Shaw <kms15@case.edu>
2013-06-28 00:51:25 +0000

    added function to read positions from scm file

--- kicadSchematicPosToPCB.py
+++ kicadSchematicPosToPCB.py
@@ -1,5 +1,31 @@
 #!/usr/bin/python3
 import sys
+import re
+
+def readPositionsFromSchematic(filename):
+    with open(filename, 'rt') as f:
+        positions = {}
+
+        reComponentLabels = re.compile(r"L +(\w+) +(\w+)");
+        reComponentPosition = re.compile(r"P +([0-9]+) +([0-9]+)");
+        reEndComp = re.compile(r"\$EndComp");
+        lastPosition = (0, 0)
+        lastRef = 'missing ref'
+
+        for line in f:
+            match = reComponentLabels.match(line)
+            if match:
+                lastRef = match.group(2)
+            else:
+                match = reComponentPosition.match(line)
+                if match:
+                    lastPosition = (int(match.group(1)), int(match.group(2)))
+                else:
+                    match = reEndComp.match(line)
+                    if match:
+                        positions[lastRef] = lastPosition
+        return positions
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 2 or len(sys.argv) > 4:
@@ -7,18 +33,19 @@ if __name__ == "__main__":
         print("{0} <schematic file> [<board file> [output file]]".format(sys.argv[0]))
         sys.exit(1);
 
-    scm_filename = sys.argv[1];
+    sch_filename = sys.argv[1];
 
     if len(sys.argv) > 2:
         brd_filename = sys.argv[2]
-    elif scm_filename[-4:] == ".sch":
-        brd_filename = scm_filename[:-4] + ".brd"
+    elif sch_filename[-4:] == ".sch":
+        brd_filename = sch_filename[:-4] + ".brd"
     else:
-        brd_filename = scm_filename + ".brd"
+        brd_filename = sch_filename + ".brd"
 
     if len(sys.argv) > 3:
         out_filename = sys.argv[3]
     else:
         out_filename = brd_filename
 
-    print("{0} + {1} -> {2}".format(scm_filename, brd_filename, out_filename));
+    print("{0} + {1} -> {2}".format(sch_filename, brd_filename, out_filename))
+    print(readPositionsFromSchematic(sch_filename))
